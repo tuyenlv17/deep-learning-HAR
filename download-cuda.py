0deff75ebf9b27899902481b0fabc658b14bda1e
Tuyen Luong <tuyenlv17@gmail.com>
2017-12-16 16:18:52 +0700

    fix setup lab qwiklab

--- download-cuda.py
+++ download-cuda.py
@@ -1,6 +1,10 @@
 import urllib
 import os
+import sys
+def dlProgress(count, blockSize, totalSize):
+	percent = int(count*blockSize*100/totalSize)
+	sys.stdout.write("\r%2d%%" % percent)
+	sys.stdout.flush()
 url=raw_input("cuda_url=")
-print url
 filename = "tools-bin/cuda_9.1.85_387.26_linux.run"
-urllib.urlretrieve (url, filename)
+urllib.urlretrieve (url, filename, reporthook=dlProgress)
---
+++ setup-qwiklab.sh
@@ -0,0 +1,23 @@
+git config --global alias.ss 'status -s'
+pip install --user tensorflow-gpu
+pip install --user pandas
+pip install --user sklearn
+pip install --user matplotlib
+
+#setup
+mkdir -p tools-bin/cuda
+mkdir -p tools-bin/cudnn
+cat tools/cudnn/cudnn-part* > tools-bin/cudnn/cudnn-8.0-linux-x64-v6.0.tgz
+tar xvf tools-bin/cudnn/cudnn-8.0-linux-x64-v6.0.tgz -C tools-bin/cudnn
+CUR_DIR=$(pwd)
+CUDA_DIR="$CUR_DIR/tools-bin/cuda"
+echo "install cuda to this dir $CUDA_DIR\n"
+python download-cuda.py
+bash ./tools-bin/cuda_9.1.85_387.26_linux.run
+mv tools-bin/cudnn/cuda/include/cudnn.h tools-bin/cuda/include/
+mv tools-bin/cudnn/cuda/lib64/* tools-bin/cuda/lib64/
+# env
+echo "add to profile export PATH=\"$CUDA_DIR/bin:\$PATH\""
+echo "export PATH=\"$CUDA_DIR/bin:\$PATH\"" >> ~/.profile
+echo "export LD_LIBRARY_PATH=\"$CUDA_DIR/lib64:\$LD_LIBRARY_PATH\"" >> ~/.profile
+. ~/.profile
