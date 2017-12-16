549746788241ff8c8e1025095bc15b11bd50a241
Tuyen Luong <tuyenlv17@gmail.com>
2017-12-17 05:08:04 +0700

    fix tp gpu

--- HAR-CNN-GPU.py
+++ HAR-CNN-GPU.py
@@ -4,6 +4,92 @@
 # # HAR CNN training 
 
 # In[292]:
+# HAR classification 
+# Author: Burak Himmetoglu
+# 8/15/2017
+
+import pandas as pd 
+import numpy as np
+import os
+
+NUM_CLASS = 18
+WINDOWN_SIZE = 128
+CHANNEL_LIST = ["x_sensor_acc","y_sensor_acc","z_sensor_acc","x_watch_acc","y_watch_gyr","z_watch_acc","x_watch_gyr","y_watch_acc","z_watch_gyr"]
+NUM_CHANNEL = len(CHANNEL_LIST)
+
+class_label = ["wrist","ankle","shoulder","haunch","knee","walking","running","kicking","cycling","brushing","washing_hand","wipe","sweep_floor","peel","slice","mixing","upstair","downstair"]
+class_label_int = {val:str(idx) for idx, val in enumerate(class_label)}
+
+def read_data(data_path, split = "train"):
+    """ Read data """
+
+    # Fixed params
+    n_class = 18
+    n_steps = WINDOWN_SIZE
+
+    # Paths
+    path_ = os.path.join(data_path, split)
+    path_signals = os.path.join(path_, "sensor")
+
+    # Read labels and one-hot encode
+    label_path = os.path.join(path_, "class.txt")
+    labels = pd.read_csv(label_path, header = None)
+    
+    # Read time-series data
+    channel_files = os.listdir(path_signals)
+    channel_files.sort()
+    n_channels = len(channel_files)
+    posix = len(split) + 5
+
+    # Initiate array
+    list_of_channels = []
+    X = np.zeros((len(labels), n_steps, n_channels))
+    i_ch = 0
+    for fil_ch in channel_files:
+        channel_name = fil_ch[:-4]
+        print channel_name
+        dat_ = pd.read_csv(os.path.join(path_signals,fil_ch), delim_whitespace = True, header = None)
+        X[:,:,i_ch] = dat_.as_matrix()
+
+        # Record names
+        list_of_channels.append(channel_name)
+
+        # iterate
+        i_ch += 1
+
+    # Return 
+    return X, labels[0].values, list_of_channels
+
+def standardize(train, test):
+    """ Standardize data """
+
+    # Standardize train and test
+    X_train = (train - np.mean(train, axis=0)[None,:,:]) / np.std(train, axis=0)[None,:,:]
+    X_test = (test - np.mean(test, axis=0)[None,:,:]) / np.std(test, axis=0)[None,:,:]
+
+    return X_train, X_test
+
+def one_hot(labels, n_class = 18):
+    """ One-hot encoding """
+    expansion = np.eye(n_class)
+    y = expansion[:, labels-1].T
+    assert y.shape[1] == n_class, "Wrong number of labels!"
+
+    return y
+
+def get_batches(X, y, batch_size = 100):
+    """ Return a generator for batches """
+    n_batches = len(X) // batch_size
+    X, y = X[:n_batches*batch_size], y[:n_batches*batch_size]
+
+    # Loop over batches and yield
+    for b in range(0, len(X), batch_size):
+        yield X[b:b+batch_size], y[b:b+batch_size]
+    
+
+
+
+
 
 
 # Imports
@@ -12,7 +98,7 @@ import os
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import precision_score,     recall_score, confusion_matrix, classification_report,     accuracy_score, f1_score
-from utils import utilities
+# from utils.utilities import *
 
 
 # In[293]:
