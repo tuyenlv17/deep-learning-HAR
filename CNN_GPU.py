bc059da62616a71f844bd08766607eca67189b48
Tuyen Luong <tuyenlv17@gmail.com>
2018-01-02 21:52:01 +0700

    recontruct gpu training

--- CNN_GPU.py
+++ CNN_GPU.py
@@ -10,7 +10,11 @@
 
 import pandas as pd 
 import numpy as np
+import tensorflow as tf
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import precision_score,     recall_score, confusion_matrix, classification_report,     accuracy_score, f1_score
 
 WINDOWN_SIZE = 128
 CHANNEL_LIST = ["x_sensor_acc","y_sensor_acc","z_sensor_acc","x_watch_acc","y_watch_gyr","z_watch_acc","x_watch_gyr","y_watch_acc","z_watch_gyr"]
@@ -18,8 +22,21 @@ NUM_CHANNEL = len(CHANNEL_LIST)
 
 class_label = ["wrist","ankle","shoulder","haunch","knee","walking","running","kicking","cycling","brushing","washing_hand","sweep_floor","peel","slice","mixing","upstair","downstair"]
 class_label=["wrist","ankle","shoulder","haunch","knee","walking","running","kicking","cycling","brushing","washing_hand","wipe","sweep_floor","peel","slice","mixing","upstair","downstair"]
+
+class_label_vn = [u"Cổ tay",u"Cổ chân",u"Bả vai",u"Xoay hông",u"Xoay đầu gối",u"Đi bộ",u"Chạy",u"Đá bóng",u"Đạp xe",u"Đánh răng",u"Rửa tay", u"Lau bàn", u"Quét nhà", u"Nạo",u"Thái",u"Trộn",u"Lên cầu thang",u"Xuống cầu thang"]
+
 NUM_CLASS = len(class_label)
 class_label_int = {val:str(idx) for idx, val in enumerate(class_label)}
+
+
+# network hyperparametter
+batch_size = 600       # Batch size
+seq_len = WINDOWN_SIZE          # Number of steps or window size
+learning_rate = 0.0001
+epochs = 1000
+
+n_classes = NUM_CLASS
+n_channels = NUM_CHANNEL
 
 def read_data(data_path, split = "train"):
     """ Read data """
@@ -87,172 +104,68 @@ def get_batches(X, y, batch_size = 100):
     for b in range(0, len(X), batch_size):
         yield X[b:b+batch_size], y[b:b+batch_size]
     
+graph = tf.Graph()
 
+# Construct placeholders
+with graph.as_default():
+    inputs_ = tf.placeholder(tf.float32, [None, seq_len, n_channels], name = 'inputs')
+    labels_ = tf.placeholder(tf.float32, [None, n_classes], name = 'labels')
+    keep_prob_ = tf.placeholder(tf.float32, name = 'keep')
+    learning_rate_ = tf.placeholder(tf.float32, name = 'learning_rate')
 
 
+# Build Convolutional Layers
 
+# In[302]:
 
 
-# Imports
-import numpy as np
-import os
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_recall_fscore_support
-from sklearn.metrics import precision_score,     recall_score, confusion_matrix, classification_report,     accuracy_score, f1_score
-# from utils.utilities import *
+with graph.as_default():
+    # (batch, 128, 9) --> (batch, 64, 18)
+    conv1 = tf.layers.conv1d(inputs=inputs_, filters=18, kernel_size=2, strides=1, 
+                             padding='same', activation = tf.nn.relu)
+    max_pool_1 = tf.layers.max_pooling1d(inputs=conv1, pool_size=2, strides=2, padding='same')
 
+    # (batch, 64, 18) --> (batch, 32, 36)
+    conv2 = tf.layers.conv1d(inputs=max_pool_1, filters=36, kernel_size=2, strides=1, 
+                             padding='same', activation = tf.nn.relu)
+    max_pool_2 = tf.layers.max_pooling1d(inputs=conv2, pool_size=2, strides=2, padding='same')
 
-# In[293]:
+    # (batch, 32, 36) --> (batch, 16, 72)
+    conv3 = tf.layers.conv1d(inputs=max_pool_2, filters=72, kernel_size=2, strides=1, 
+                             padding='same', activation = tf.nn.relu)
+    max_pool_3 = tf.layers.max_pooling1d(inputs=conv3, pool_size=2, strides=2, padding='same')
 
+    # (batch, 16, 72) --> (batch, 8, 144)
+    conv4 = tf.layers.conv1d(inputs=max_pool_3, filters=144, kernel_size=2, strides=1, 
+                             padding='same', activation = tf.nn.relu)
+    max_pool_4 = tf.layers.max_pooling1d(inputs=conv4, pool_size=2, strides=2, padding='same')
 
 
-# In[294]:
+# Now, flatten and pass to the classifier
 
-def train():
-    class_label_vn = [u"Cổ tay",u"Cổ chân",u"Bả vai",u"Xoay người",u"Xoay đầu gối",u"Đi bộ",u"Chạy",u"Đạp xe",u"Đánh răng",u"Rửa tay",u"Thái",u"Trộn",u"Lên cầu thang",u"Xuống cầu thang"]
+# In[303]:
 
 
-    # ## Prepare data
+with graph.as_default():
+    # Flatten and add dropout
+    flat = tf.reshape(max_pool_4, (-1, 8*144))
+    flat = tf.nn.dropout(flat, keep_prob=keep_prob_)
 
-    # In[295]:
+    # Predictions
+    logits = tf.layers.dense(flat, n_classes)
 
+    # Cost function and optimizer
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels_))
+    optimizer = tf.train.AdamOptimizer(learning_rate_).minimize(cost)
 
-    rootDatasetDir = "./datasets/PTIT/normalized"
-    X_train, labels_train, list_ch_train = read_data(data_path="./datasets/PTIT/normalized", split="train") # train
-    X_test, labels_test, list_ch_test = read_data(data_path=rootDatasetDir, split="test") # test
-    assert list_ch_train == list_ch_test, "Mistmatch in channels!"
-
-
-    # In[296]:
-
-
-    # Normalize?
-    X_train, X_test = standardize(X_train, X_test)
-
-
-    # Train/Validation Split
-
-    # In[297]:
-
-
-    #X_tr, X_vld, lab_tr, lab_vld = train_test_split(X_train, labels_train, 
-    #                                                stratify = labels_train, random_state = 123)
-    X_tr=X_train
-    lab_tr=labels_train
-
-    # One-hot encoding:
-
-    # In[298]:
-
-
-    y_tr = one_hot(lab_tr)
-    #y_vld = one_hot(lab_vld)
-    y_test = one_hot(labels_test)
-
-
-    # In[299]:
-
-
-    # Imports
-    import tensorflow as tf
-
-
-    # ### Hyperparameters
-
-    # In[300]:
-
-
-    batch_size = 600       # Batch size
-    seq_len = WINDOWN_SIZE          # Number of steps or window size
-    learning_rate = 0.0001
-    epochs = 1000
-
-    n_classes = NUM_CLASS
-    n_channels = NUM_CHANNEL
-    print "n_channels %d" % n_channels
-
-
-    # ### Construct the graph
-    # Placeholders
-
-    # In[301]:
-
-
-    graph = tf.Graph()
-
-    # Construct placeholders
-    with graph.as_default():
-        inputs_ = tf.placeholder(tf.float32, [None, seq_len, n_channels], name = 'inputs')
-        labels_ = tf.placeholder(tf.float32, [None, n_classes], name = 'labels')
-        keep_prob_ = tf.placeholder(tf.float32, name = 'keep')
-        learning_rate_ = tf.placeholder(tf.float32, name = 'learning_rate')
-
-
-    # Build Convolutional Layers
-
-    # In[302]:
-
-
-    with graph.as_default():
-        # (batch, 128, 9) --> (batch, 64, 18)
-        conv1 = tf.layers.conv1d(inputs=inputs_, filters=18, kernel_size=2, strides=1, 
-                                 padding='same', activation = tf.nn.relu)
-        max_pool_1 = tf.layers.max_pooling1d(inputs=conv1, pool_size=2, strides=2, padding='same')
-
-        # (batch, 64, 18) --> (batch, 32, 36)
-        conv2 = tf.layers.conv1d(inputs=max_pool_1, filters=36, kernel_size=2, strides=1, 
-                                 padding='same', activation = tf.nn.relu)
-        max_pool_2 = tf.layers.max_pooling1d(inputs=conv2, pool_size=2, strides=2, padding='same')
-
-        # (batch, 32, 36) --> (batch, 16, 72)
-        conv3 = tf.layers.conv1d(inputs=max_pool_2, filters=72, kernel_size=2, strides=1, 
-                                 padding='same', activation = tf.nn.relu)
-        max_pool_3 = tf.layers.max_pooling1d(inputs=conv3, pool_size=2, strides=2, padding='same')
-
-        # (batch, 16, 72) --> (batch, 8, 144)
-        conv4 = tf.layers.conv1d(inputs=max_pool_3, filters=144, kernel_size=2, strides=1, 
-                                 padding='same', activation = tf.nn.relu)
-        max_pool_4 = tf.layers.max_pooling1d(inputs=conv4, pool_size=2, strides=2, padding='same')
-
-
-    # Now, flatten and pass to the classifier
-
-    # In[303]:
-
-
-    with graph.as_default():
-        # Flatten and add dropout
-        flat = tf.reshape(max_pool_4, (-1, 8*144))
-        flat = tf.nn.dropout(flat, keep_prob=keep_prob_)
-
-        # Predictions
-        logits = tf.layers.dense(flat, n_classes)
-
-        # Cost function and optimizer
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels_))
-        optimizer = tf.train.AdamOptimizer(learning_rate_).minimize(cost)
-
-        # Accuracy
-        correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(labels_, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='accuracy')
-
-
-    # ### Train the network
-
-    # In[ ]:
-
-    # In[ ]:
-
-
-    validation_acc = []
-    validation_loss = []
-
-    train_acc = []
-    train_loss = []
-
-    with graph.as_default():
-        saver = tf.train.Saver()
-
+    # Accuracy
+    correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(labels_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='accuracy')
+    
+with graph.as_default():
+    saver = tf.train.Saver()
+        
+def train_model(graph, X_tr, y_tr):    
     with tf.Session(graph=graph) as sess:
         sess.run(tf.global_variables_initializer())
         iteration = 1
@@ -268,26 +181,16 @@ def train():
 
                 # Loss
                 loss, _ , acc = sess.run([cost, optimizer, accuracy], feed_dict = feed)
-                train_acc.append(acc)
-                train_loss.append(loss)
+#                 train_acc.append(acc)
+#                 train_loss.append(loss)
                 # Iterate 
                 iteration += 1
 
         saver.save(sess,"checkpoints-cnn/har.ckpt")
 
 
-    # In[ ]:
-
-
-    # Plot training and test loss
-    t = np.arange(iteration-1)
-
-
-    # ## Evaluate on test set
-
-    # In[ ]:
-
-
+def test_model(graph, X_test, y_test):
+    global accuracy
     test_acc = []
     with graph.as_default():
         prediction=tf.argmax(logits,1)
@@ -308,7 +211,7 @@ def train():
     #     print y_true
     #     print y_pred
         sk_class_labels = [i for i in range(NUM_CLASS)]
-        accuracy = accuracy_score(y_true, y_pred)
+        accuracy1 = accuracy_score(y_true, y_pred)
         precision = precision_score(y_true, y_pred, average='macro')
         recall = recall_score(y_true, y_pred, average='macro')
         f1 = f1_score(y_true, y_pred, average='macro')
@@ -319,4 +222,50 @@ def train():
         print 'Precision:', precision
         print '\n clasification report:\n', classification_report(y_true,y_pred,digits=5)
         print '\n confussion matrix:\n',confusion_matrix(y_true, y_pred)
-        return accuracy, precision, recall, f1
+        return accuracy1, precision, recall, f1
+        
+def getPredicted(graph, X_test):
+    global accuracy
+    test_acc = []
+    with graph.as_default():
+        prediction=tf.argmax(logits,1)
+    with tf.Session(graph=graph) as sess:
+        # Restore
+        saver.restore(sess, tf.train.latest_checkpoint('checkpoints-cnn'))
+        y_pred = []
+        y_true = []
+        feed = {inputs_: X_test,
+                keep_prob_: 1}
+        batch_y_pred = sess.run([prediction], feed_dict=feed)
+    return batch_y_pred
+def train():
+    global graph
+    rootDatasetDir = "./datasets/PTIT/normalized"
+    X_train, labels_train, list_ch_train = read_data(data_path="./datasets/PTIT/normalized", split="train") # train
+    X_test, labels_test, list_ch_test = read_data(data_path=rootDatasetDir, split="test") # test
+    assert list_ch_train == list_ch_test, "Mistmatch in channels!"
+
+#     X_train, X_test = standardize(X_train, X_test)
+
+    X_tr=X_train
+    lab_tr=labels_train
+
+
+
+    y_tr = one_hot(lab_tr)
+    y_test = one_hot(labels_test)
+
+
+    validation_acc = []
+    validation_loss = []
+
+    train_acc = []
+    train_loss = []
+    
+#     graph = build_net()
+    print "training model"
+    train_model(graph, X_tr, y_tr)
+    print "load model and test"
+    test_model(graph, X_test, y_test)
+    print getPredicted(graph, X_test)
+train()
