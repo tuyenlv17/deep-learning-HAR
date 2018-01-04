ab381f854cdb7faac310760e8b75f48251f2dde1
Tuyen Luong <tuyenlv17@gmail.com>
2018-01-04 23:27:05 +0700

    fix cuda download direct

--- download-cuda.py
+++ download-cuda.py
@@ -1,10 +1,6 @@
 import urllib
 import os
 import sys
-def dlProgress(count, blockSize, totalSize):
-	percent = int(count*blockSize*100/totalSize)
-	sys.stdout.write("\r%2d%%" % percent)
-	sys.stdout.flush()
-url=raw_input("cuda_url=")
+url='https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_387.26_linux'
 filename = "tools-bin/cuda_9.1.85_387.26_linux.run"
 urllib.urlretrieve (url, filename, reporthook=dlProgress)
