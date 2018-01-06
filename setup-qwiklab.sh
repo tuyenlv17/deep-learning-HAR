63b54df8bf87962c3923f41b0c59d0947169a9b8
Tuyen Luong <tuyenlv17@gmail.com>
2018-01-06 23:12:04 +0700

    fix script

--- setup-qwiklab.sh
+++ setup-qwiklab.sh
@@ -5,19 +5,21 @@ pip install --user sklearn
 pip install --user matplotlib
 
 #setup
-mkdir -p tools-bin/cuda
+mkdir -p tools-bin/cuda/lib64
+mkdir -p tools-bin/cuda/include
 mkdir -p tools-bin/cudnn
 cat tools/cudnn/cudnn-part* > tools-bin/cudnn/cudnn-8.0-linux-x64-v6.0.tgz
 tar xvf tools-bin/cudnn/cudnn-8.0-linux-x64-v6.0.tgz -C tools-bin/cudnn
 CUR_DIR=$(pwd)
 CUDA_DIR="$CUR_DIR/tools-bin/cuda"
 echo "install cuda to this dir $CUDA_DIR\n"
-python download-cuda.py
-bash ./tools-bin/cuda_9.1.85_387.26_linux.run
+# python download-cuda.py
+# bash ./tools-bin/cuda_9.1.85_387.26_linux.run
 mv tools-bin/cudnn/cuda/include/cudnn.h tools-bin/cuda/include/
 mv tools-bin/cudnn/cuda/lib64/* tools-bin/cuda/lib64/
 # env
 echo "add to profile export PATH=\"$CUDA_DIR/bin:\$PATH\""
 echo "export PATH=\"$CUDA_DIR/bin:\$PATH\"" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=\"$CUDA_DIR/lib64:\$LD_LIBRARY_PATH\"" >> ~/.bashrc
+# echo "export LD_LIBRARY_PATH=\"$CUDA_DIR/lib64:\$LD_LIBRARY_PATH\"" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=\"/notebooks/deep-learning-HAR/tools-bin/cuda/lib64:/usr/local/cuda-8.0/lib64:\$LD_LIBRARY_PATH\"" >> ~/.bashrc
 . ~/.bashrc
